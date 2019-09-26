5128a16d7f860fb32b8ac938ce68b8fbd315d411
Nicole B <48190415+nicbatten@users.noreply.github.com>
2019-09-25 22:41:36 -0400

    Added functionality to write file to disk

--- download_file.py
+++ download_file.py
@@ -4,5 +4,7 @@ import requests
 def download(url):
     get_response = requests.get(url)
     print(get_response.content)
+    with open("sample.txt", "w") as out_file:
+        out_file.write("This is a test")
 
 download("https://media1.tenor.com/images/fa395dfc3172219eee7bc528b07f29b4/tenor.gif?itemid=4420482")
