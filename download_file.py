8bf76482c73325a43dce10e8bf02f5239cda8ca2
Nicole B <48190415+nicbatten@users.noreply.github.com>
2019-09-25 22:52:14 -0400

    Improved file download functionality

--- download_file.py
+++ download_file.py
@@ -3,8 +3,10 @@ import requests
 
 def download(url):
     get_response = requests.get(url)
-    print(get_response.content)
-    with open("sample.txt", "w") as out_file:
-        out_file.write("This is a test")
+    file_name = url.split("/")[-1]
+    #print(get_response.content)
+    print(file_name)
+    with open(file_name, "wb") as out_file:
+        out_file.write(get_response.content)
 
-download("https://media1.tenor.com/images/fa395dfc3172219eee7bc528b07f29b4/tenor.gif?itemid=4420482")
+download("https://media1.tenor.com/images/fa395dfc3172219eee7bc528b07f29b4/tenor.gif")
